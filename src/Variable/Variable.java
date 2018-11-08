a4d6c69ae3d4fadf0b45ee47d5ad895d1e1f80fa
mrkadir69 <1810653260@stud.fh-kufstein.ac.at>
2018-11-08 14:00:42 +0100

    Variable

---
+++ src/Variable/Variable.java
@@ -0,0 +1,14 @@
+package Variable;
+
+public class Variable
+{
+    public static void main (String[] args)
+    {
+        String name;
+        name = "Kadir";
+        System.out.println("" + name);
+        int age;
+        age = 18;
+        System.out.println(""+age);
+    }
+}
